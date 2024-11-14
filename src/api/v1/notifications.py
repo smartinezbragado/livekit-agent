c5c1653c8b6d17fbdb292cf6829ed6146751240f
smartinezbragado <smartinezbragado@gmail.com>
2024-11-14 19:21:26 +0100

    feat: use aws ses service for notifications

--- src/api/v1/notifications.py
+++ src/api/v1/notifications.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from src.api.v1.schemas import EmailRequest
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+import boto3
 import os
 from loguru import logger
 
@@ -10,24 +9,29 @@ router = APIRouter(prefix="/api/v1/notifications")
 
 @router.post("/send-email")
 async def send_email(request: EmailRequest):
-    SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-    FROM_EMAIL = os.getenv("FROM_EMAIL")
 
-    if not SENDGRID_API_KEY or not FROM_EMAIL:
-        raise HTTPException(status_code=500, detail="SendGrid not configured")
-
-    message = Mail(
-        from_email=FROM_EMAIL,
-        to_emails=request.email,
-        subject=request.subject,
-        plain_text_content=request.content
-    )
-
+    # Create an SES client
+    ses_client = boto3.client('ses', region_name='eu-north-1')
     try:
-        sg = SendGridAPIClient(SENDGRID_API_KEY)
-        response = sg.send(message)
-        logger.warning(response.body)
+        response = ses_client.send_email(
+            Source=os.environ["SENDER_EMAIL"],
+            Destination={
+                'ToAddresses': [request.email],
+            },
+            Message={
+                'Subject': {
+                    'Data': request.subject,
+                },
+                'Body': {
+                    'Text': {
+                        'Data': request.content,
+                    },
+                },
+            }
+        )
+        logger.warning(response)
     except Exception as e:
+        logger.error(str(e))
         raise HTTPException(status_code=500, detail=str(e))
 
     return {"message": f"Email sent to {request.email}"}
